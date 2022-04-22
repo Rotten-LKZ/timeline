b7498609bbaaef6a77099dfff6dbc82fe63afe49
Rotten-LKZ <Rotten_LKZ@163.com>
2022-04-22 21:42:06 +0800

    feat: init

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const timeline = document.getElementById('timeline')
+if (timeline.getContext) {
+  const ctx = timeline.getContext('2d')
+}
---
+++ times.js
@@ -0,0 +1,4 @@
+const times = [
+  { time: '2022.4.20', msg: 'Got the idea for the first time' },
+  { time: '2022.4.22', msg: 'Try archieving the idea' },
+]
